e6c7a75b59e6b29238ea7cb8cb3522ff0fa3772a
georgrybski <rybskigeorg@gmail.com>
2023-01-25 15:35:52 -0300

    Added IPhoneFactory interface

---
+++ src/br/com/ada/georg/oop2/iphonefactoryexercise/factories/IPhoneFactory.java
@@ -0,0 +1,7 @@
+package br.com.ada.georg.oop2.iphonefactoryexercise.factories;
+
+import br.com.ada.georg.oop2.iphonefactoryexercise.iphones.IPhone;
+
+public interface IPhoneFactory {
+    IPhone createIphone();
+}
